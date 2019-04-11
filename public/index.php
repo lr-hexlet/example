9a200feed72d27c76d174ebecc4664f8cc0ea967
lr-hexlet <lenri-hexlet@mail.ru>
2019-04-11 10:52:12 +0300

    New pages

--- public/index.php
+++ public/index.php
@@ -15,4 +15,12 @@ $app->get('/', function ($request, $response) {
 	return $response->write('Welcome to Slim!'); 
 }); 
 
+$app->get('/users', function ($request, $response) { 
+	return $response->write('GET: users'); 
+}); 
+
+$app->post('/users', function ($request, $response) { 
+	return $response->write('POST: users'); 
+}); 
+
 $app->run();
