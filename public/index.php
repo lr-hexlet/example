4d2ee127f8ad0ac2ef7a990d8300af04c19e44dd
lr-hexlet <lenri-hexlet@mail.ru>
2019-04-16 23:40:50 +0300

    New pages courses

--- public/index.php
+++ public/index.php
@@ -15,10 +15,17 @@ $app = new \Slim\App($configuration);
 $container = $app->getContainer();
 $container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 
+$courses = [['id' => 1, 'name' => 'cours 1'],['id' => 2, 'name' => 'cours 2']];
 $app->get('/users/{id}', function($request, $response, $args) {
 	$params = ['id' => $args['id']];
 	return $this->renderer->render($response, 'users/show.phtml', $params);
 });
+
+$app->get('/courses', function($request, $response) use ($courses) {
+	$params = ['courses' => $courses ];
+	return $this->renderer->render($response, 'courses/index.phtml', $params);
+});
+
 $app->get('/', function ($request, $response) { 
 	return $response->write('Welcome to Slim!'); 
 }); 
